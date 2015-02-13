40bfc304b1fe8ebc66a2800b41aff2bec33c4f73
Andreas Gullberg Larsen <anjdreas@gmail.com>
2015-02-13 20:45:22 +0100

    core: Add NoOpLogFactory

---
+++ Src/PortableLog.Core/NoOpLogFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PortableLog.Core
+{
+    public class NoOpLogFactory : ILogFactory
+    {
+        public ILogEx GetLogger(string loggerName)
+        {
+            return new NoOpLoggerEx();
+        }
+
+        public ILogEx GetLogger(Type type)
+        {
+            return new NoOpLoggerEx();
+        }
+
+        public ILogEx GetLogger<T>()
+        {
+            return new NoOpLoggerEx();
+        }
+    }
+}
