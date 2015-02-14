d4542a5a3b2cf9f52757c48187561eab69a6f541
Andreas Gullberg Larsen <anjdreas@gmail.com>
2015-02-14 01:31:59 +0100

    core: Add StringExtensions.With()

---
+++ Src/PortableLog.Core/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace PortableLog.Core
+{
+    public static class StringExtensions
+    {
+        [StringFormatMethod("format")]
+        public static string With(this string format, IFormatProvider provider, params object[] args)
+        {
+            return string.Format(provider, format, args);
+        }
+
+        [StringFormatMethod("format")]
+        public static string With(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+    }
+}
